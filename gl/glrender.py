00249744b0431b331e07bd4c5ee229cd23675742
eleanor11 <xll9400@gmail.com>
2018-03-10 18:28:50 +0800

    change window position

--- gl/glrender.py
+++ gl/glrender.py
@@ -39,7 +39,7 @@ class GLRenderer(object):
 		glutInit()
 		displayMode = GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH | GLUT_STENCIL
 		glutInitDisplayMode(displayMode)
-		glutInitWindowPosition(0, 0)
+		glutInitWindowPosition(1920, 0)
 		glutInitWindowSize(self.width, self.height)
 		self.window = glutCreateWindow(name)
 		glEnable(GL_CULL_FACE)
